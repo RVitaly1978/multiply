aafae962a37d6ef4dd2f095d5942a3368449dd02
Vitaly Raicheu <RVitaly1978@gmail.com>
2019-10-03 19:33:41 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let number_first = BigInt(first);
+  let number_second = BigInt(second);
+  let multi = number_first * number_second;
+  return String(multi);
 }
+
+// module.exports = function multiply(first, second) {
+//   let number_first = parseInt(first, 10);
+//   let number_second = parseInt(second, 10);
+//   let multi = number_first * number_second;
+//   return String(multi);
+// }
